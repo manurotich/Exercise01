52cc65c7383d10a12436a45b48981f236f9f721f
Mod <rotichmanuela@gmail.com>
2021-07-31 10:19:19 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Exercise01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Sample Email Regex: ^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$
+
+            CheckValue();
+        }
+
+        //Regex Checker Class
+        private static void CheckValue()
+        {
+            //Takes an Email as the correct input
+            string InputEmail_Value;
+
+            //Prompt user to enter Regular Expression
+            Console.WriteLine("The default expression to check an email.");
+            Console.Write("Enter a regular Expression (or press ENTER to use the default): ");
+            Regex regex = new Regex(Console.ReadLine());
+
+            //Prompt user to enter Email
+            Console.Write("Enter some input: ");
+            InputEmail_Value = Console.ReadLine();
+
+            //Run Regex Comparison
+            Match match = regex.Match(InputEmail_Value);
+
+            //Output results
+            if (match.Success)
+            {
+                Console.WriteLine(InputEmail_Value + " Matches " + regex.ToString() + "? True");
+            }
+
+            else
+            {
+                Console.WriteLine(InputEmail_Value + " Matches " + regex.ToString() + "? False");
+            }
+        }
+    }
+}
